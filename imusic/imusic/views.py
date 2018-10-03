21781c007bcaa8f0f149f86da9e4857bac5c0a19
Your Name <you@example.com>
2018-10-03 20:21:52 +0800

    change

---
+++ imusic/imusic/views.py
@@ -0,0 +1,21 @@
+#-*- coding: UTF-8 -*-
+import mysql.connector
+
+from django.http import HttpResponse
+import json
+#
+# conn = mysql.connector.connect(user='root', password='826209886', database='test_song')
+#
+# cursor = conn.cursor()
+# cursor.execute("select singer_name from song_sex where sex = '%s'" % '男')
+# values = cursor.fetchall()
+# list=[]
+# for row in values:
+#     print(row[0])
+#     list.append(row[0])
+# cursor.close()
+# conn.close()
+
+def ajax_list(request):
+    a = range(100)
+    return HttpResponse(json.dumps(a), content_type='application/json')

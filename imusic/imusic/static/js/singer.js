787493a53fd5f1bfe48d27a477bd003a5669866f
Your Name <you@example.com>
2018-10-03 15:38:05 +0800

    change

--- imusic/imusic/static/js/singer.js
+++ imusic/imusic/static/js/singer.js
@@ -1,6 +1,6 @@
-var aArr=$(".tabBox").find("a");
-var len=aArr.length;
 window.onload=function () {
+    var aArr=$(".tabBox").find("a");
+    var len=aArr.length;
     for(var i=0;i<len;i++)
     {
         aArr[i].onclick=change1;//为每个a标签的单击事件赋函数，注意是arr[i].而不是aArr
@@ -10,68 +10,71 @@ window.onload=function () {
     $("#hotsong").css("color",'#3effad');
     $("#hotsong").css("font-weight",'800');
     $(".hot").css("background-color",'#63ffda');
-};
-$('.homemenu li').hover(function(){
-    $(this).find('ul').show();
-},function(){
-    $(this).find('ul').hide();
-    /*$(".more").find('a[class^="selected"]').attr("class","");*/
-});
-$(".nav__item a").click(function() {
-    $(".nav__item a").css('color','#000');
-    $(this).css("color","#3effad");
-    $(".nav__item a").css('font-weight','');
-    $(this).css("font-weight","900");
-});
-function change1(event) {
-    var index=event.target.dataset.index;
-    for(var j=0;j<len;j++){
-        if(j==index){
-            aArr[j].className="selected";
-            aArr[j].id="unchange";
-        }
-        else{
-            aArr[j].className="";
-        }
-    }
-    return index;
-}
-function change2(event) {
-    var index=event.target.dataset.index;
-    for(var j=0;j<len;j++){
-        if(j==index){
-            aArr[j].className="selected";
-            aArr[j].id="change";
-        }
-        else{
-            aArr[j].id="unchange";
-        }
-    }
-}
 
-function nochange(event) {
-    var index=event.target.dataset.index;
-    if(index==4){
-        /*event.stopPropagation();*/
-    }
-    else{
+    $('.homemenu li').hover(function(){
+        $(this).find('ul').show();
+    },function(){
+        $(this).find('ul').hide();
+        /*$(".more").find('a[class^="selected"]').attr("class","");*/
+    });
+    $(".nav__item a").click(function() {
+        $(".nav__item a").css('color','#000');
+        $(this).css("color","#3effad");
+        $(".nav__item a").css('font-weight','');
+        $(this).css("font-weight","900");
+    });
+    $(".index a").click(function(){
+        $(".index a").css('background-color','')
+        $(this).css("background-color","#63ffda");
+    });
+    $(".area_item a").click(function(){
+        $(".area_item a").css('background-color','')
+        $(this).css("background-color","#63ffda");
+    });
+    $(".sex a").click(function(){
+        $(".sex a").css('background-color','')
+        $(this).css("background-color","#63ffda");
+    });
+
+    function change1(event) {
+        var index=event.target.dataset.index;
         for(var j=0;j<len;j++){
-            if(aArr[j].id=="change"){
+            if(j==index){
+                aArr[j].className="selected";
+                aArr[j].id="unchange";
+            }
+            else{
                 aArr[j].className="";
+            }
+        }
+        return index;
+    }
+    function change2(event) {
+        var index=event.target.dataset.index;
+        for(var j=0;j<len;j++){
+            if(j==index){
+                aArr[j].className="selected";
+                aArr[j].id="change";
+            }
+            else{
                 aArr[j].id="unchange";
             }
         }
     }
-}
-$(".index a").click(function(){
-    $(".index a").css('background-color','')
-    $(this).css("background-color","#63ffda");
-});
-$(".area_item a").click(function(){
-    $(".area_item a").css('background-color','')
-    $(this).css("background-color","#63ffda");
-});
-$(".sex a").click(function(){
-    $(".sex a").css('background-color','')
-    $(this).css("background-color","#63ffda");
-});
+
+    function nochange(event) {
+        var index=event.target.dataset.index;
+        if(index==4){
+            /*event.stopPropagation();*/
+        }
+        else{
+            for(var j=0;j<len;j++){
+                if(aArr[j].id=="change"){
+                    aArr[j].className="";
+                    aArr[j].id="unchange";
+                }
+            }
+        }
+    }
+
+};
